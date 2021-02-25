979290e99ecea88de5813f26e9316414d6641572
RomanovIgnat <romanov.ignat.p@gmail.com>
2021-02-25 05:56:49 +0300

    trash

--- train.py
+++ train.py
@@ -13,8 +13,8 @@ from learner import Learner
 writer = SummaryWriter()
 r = 5
 for i in range(100):
-    writer.add_scalars('run_14h', {'xsinx':i*np.sin(i/r),
-                                    'xcosx':i*np.cos(i/r),
+    writer.add_scalars('run_14h', {'xsinx':(i + 1) * np.sin(i/r),
+                                    'xcosx':(i + 1) * np.cos(i/r),
                                     'tanx': np.tan(i/r)}, i)
 writer.close()
 
