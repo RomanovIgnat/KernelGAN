b909238adafa1722dbe303ab728a4fc187279854
RomanovIgnat <romanov.ignat.p@gmail.com>
2021-03-10 20:51:35 +0300

    new_folder

--- torchZSSR/torchZSSR.py
+++ torchZSSR/torchZSSR.py
@@ -234,6 +234,7 @@ class ZSSR:
         # Run network
         with torch.no_grad():
             print(torch.cuda.memory_summary())
+            torch.cuda.empty_cache()
             res = self.model(interpolated_lr_son)
         return np.clip(np.squeeze(self.model(res).cpu().detach().permute(0, 2, 3, 1).numpy()), 0, 1)
 
