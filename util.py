f6563893595f94e2ac816ce451bb5e407c785ceb
RomanovIgnat <romanov.ignat.p@gmail.com>
2021-03-25 01:56:26 +0300

    fix_random

--- util.py
+++ util.py
@@ -213,9 +213,8 @@ def kernel_shift(kernel, sf):
 def save_final_kernel(k_2, conf):
     """saves the final kernel and the analytic kernel to the results folder"""
     # sio.savemat(os.path.join(conf.output_dir_path, '%s_kernel_x2.mat' % conf.img_name), {'Kernel': k_2})
-    im = Image.fromarray(k_2)
-    im = im.convert('RGB')
-    im.save(os.path.join(conf.output_dir_path, '%s_kernel_x2.png' % conf.img_name))
+    im = Image.fromarray(k_2).convert('RGB')
+    im.save(os.path.join(conf.output_dir_path, '%s_kernel_x2.jpeg' % conf.img_name))
     if conf.X4:
         k_4 = analytic_kernel(k_2)
         sio.savemat(os.path.join(conf.output_dir_path, '%s_kernel_x4.mat' % conf.img_name), {'Kernel': k_4})
