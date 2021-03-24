cf932c9854c93c29dcc2d7da022a996fdcdc874f
RomanovIgnat <romanov.ignat.p@gmail.com>
2021-03-25 01:38:42 +0300

    fix_random

--- util.py
+++ util.py
@@ -214,6 +214,7 @@ def save_final_kernel(k_2, conf):
     """saves the final kernel and the analytic kernel to the results folder"""
     # sio.savemat(os.path.join(conf.output_dir_path, '%s_kernel_x2.mat' % conf.img_name), {'Kernel': k_2})
     im = Image.fromarray(k_2)
+    im = im.convert('RGB')
     im.save(os.path.join(conf.output_dir_path, '%s_kernel_x2.png' % conf.img_name))
     if conf.X4:
         k_4 = analytic_kernel(k_2)
