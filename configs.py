e178cbf89bb8cab7afcc3cca708f38a403f2db2a
RomanovIgnat <romanov.ignat.p@gmail.com>
2021-04-14 18:44:05 +0300

    830

--- configs.py
+++ configs.py
@@ -30,8 +30,8 @@ class Config:
         self.parser.add_argument('--max_iters', type=int, default=10000, help='# of iterations')
 
         # Optimization hyper-parameters
-        self.parser.add_argument('--g_lr', type=float, default=2e-4, help='initial learning rate for generator')
-        self.parser.add_argument('--d_lr', type=float, default=2e-4, help='initial learning rate for discriminator')
+        self.parser.add_argument('--g_lr', type=float, default=5e-4, help='initial learning rate for generator')
+        self.parser.add_argument('--d_lr', type=float, default=5e-4, help='initial learning rate for discriminator')  # 2
         self.parser.add_argument('--beta1', type=float, default=0.5, help='Adam momentum')
 
         # GPU
